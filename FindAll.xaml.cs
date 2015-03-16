76093caa989e5b89fb3dca644cee337213636274
Lee Holmes <github@leeholems.com>
2015-03-15 18:09:47 -0700

    Initial commit

---
+++ FindAll.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.PowerShell.Host.ISE;
+using System.Text.RegularExpressions;
+
+namespace FindAll
+{
+    /*
+     Add-Type -Path 'd:\documents\Visual Studio Projects\FindAll\bin\Debug\FindAll.dll'
+     $psISE.CurrentPowerShellTab.VerticalAddOnTools.Add(‘Find All’, [FindAll.FindAllWindow], $true)
+    */
+
+    /// <summary>
+    /// Interaction logic for FindAll.xaml
+    /// </summary>
+    public partial class FindAllWindow : UserControl, IAddOnToolHostObject
+    {
+        public FindAllWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Populated by the ISE because we implement the IAddOnToolHostObject interface.
+        // Represents the entry-point to the ISE object model.
+        public ObjectModelRoot HostObject
+        {
+            get;
+            set;
+        }
+
+        // The user had typed new text in the search box
+        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Clear the previous results
+            this.SearchResults.Items.Clear();
+
+            // Break the file into its lines
+            string[] lineBreakers = new string[] { "\r\n" };
+            string[] fileText = HostObject.CurrentPowerShellTab.Files.SelectedFile.Editor.Text.Split(
+                lineBreakers, StringSplitOptions.None);
+
+            // Try to see if their search text represents a Regular Expression
+            Regex searchRegex = null;
+            try
+            {
+                searchRegex = new Regex(this.SearchText.Text, RegexOptions.IgnoreCase);
+            }
+            catch (ArgumentException)
+            {
+                // Ignore the ArgumentException that we get if the regular expression is
+                // not valid.
+            }
+
+            // Go through all of the lines in the file
+            for (int lineNumber = 0; lineNumber < fileText.Length; lineNumber++)
+            {
+                // See if the line matches the regex or literal text
+                if (
+                    ((searchRegex != null) && (searchRegex.IsMatch(fileText[lineNumber]))) ||
+                    (fileText[lineNumber].IndexOf(this.SearchText.Text, StringComparison.CurrentCultureIgnoreCase) >= 0))
+                {
+                    // If so, add it to the search results box.
+                    SearchResult result = new SearchResult() {
+                        Line = lineNumber + 1, Content = fileText[lineNumber]
+                    };
+                    this.SearchResults.Items.Add(result);
+                }
+            }
+        }
+
+        // Detect a double-click on an item
+        private void SearchResults_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            SelectItem();
+        }
+
+        // Detect Enter on an item
+        private void SearchResults_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SelectItem();
+            }
+        }
+
+        // Move the editor caret to the position represented by the currently-selected search
+        // result
+        private void SelectItem()
+        {
+            SearchResult selectedItem = (SearchResult)this.SearchResults.SelectedItem;
+            HostObject.CurrentPowerShellTab.Files.SelectedFile.Editor.SetCaretPosition(selectedItem.Line, 1);
+        }
+    }
+
+    // A class to hold search results
+    public class SearchResult
+    {
+        public int Line { get; set; }
+        public string Content { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0,5}: {1}", Line, Content);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # PowerShellIseFindAllPlugin
 Implementation of a "Find All" plugin for the PowerShell ISE
+
+We frequently get questions asking if the PowerShell ISE supports a feature that it doesn’t. For example,
+variable watch windows, function browsers, or “find all matches in the current document”.
+
+Or as another example, many of you are very familiar with Visual Studio and naturally wish for
+Visual Studio feature <x>. That <x> is usually different for each person :) The Visual Studio team is
+many times larger than the PowerShell team, and they’ve had a 15-year head start.
+
+When preparing for PowerShell V3, we realized that the appetite for new functionality in the ISE would far
+outstrip our ability to create it, so we designed one of the most (until now) un-heralded features in
+the PowerShell ISE: the ability to create custom tool windows.
+
+Creating a PowerShell ISE Add-In / Plugin is very straight-forward. Jason gives a great overview in his
+blog: http://blog.codeassassin.com/2012/01/16/create-a-powershell-v3-ise-add-on-tool/.
+
+The design center of ISE plugins are that they are really just WPF UserControls. In addition, they implement the IAddOnToolHostObject interface.
+
+When you call the .Add() method on either $psISE.CurrentPowerShellTab.VerticalAddOnTools or
+$psISE.CurrentPowerShellTab.HorizontalAddOnTools, the ISE will create your UserControl, set the HostObject property to an
+object that represents the ISE’s object model (almost identical to the $psISE object model variable you may be used to from
+within the ISE), and then add your control to the appropriate pane.
+
+After that, the control is all yours.
+
+You have full access to WPF, XAML, C#, and anything else you can imagine or would expect from a WPF control. This repository
+contains the implementation of a very simple "Find All" plugin for the ISE.
+
+For more information, see: http://www.leeholmes.com/blog/2013/04/04/creating-add-ons-plugins-and-tools-for-the-powershell-ise/
